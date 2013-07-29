edc62f0e5ad8fe9991ea548e2015bc4e795a78b2
Anthony Goddard <anthony@anthonygoddard.com>
2013-07-29 20:36:39 +1000

    no longer using twitter after conversion to gem

--- config-sample.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-      config.consumer_key = "key"
-      config.consumer_secret = "secret"    
-      config.oauth_token = "token"
-      config.oauth_token_secret = "tokensecret"
-end
